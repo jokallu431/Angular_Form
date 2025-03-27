edd6b9e1cb08aa3b76777ebd4aec9efd958bc215
Joshua <joshua@reflexion.ai>
2025-03-28 04:09:56 +0530

    edit component to use route parameters and implement user update functionality

--- angular_form/src/app/table/table.component.ts
+++ angular_form/src/app/table/table.component.ts
@@ -1,16 +1,15 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ViewChild, inject } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Inject, ViewChild, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { TableService, userData } from './table.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { EditComponent } from './edit/edit.component';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 /**
  * @title Table with pagination
@@ -33,8 +32,10 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableComponent implements AfterViewInit {
+  constructor(private apiservice: TableService,
+              private router: Router, 
+  ) { }
   private _snackBar = inject(MatSnackBar);
-  constructor(private apiservice: TableService) { }
   private _liveAnnouncer = inject(LiveAnnouncer);
   readonly dialog = inject(MatDialog);
   title = 'Angular_Task';
@@ -61,21 +62,34 @@ export class TableComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.fetchUser();
   }
-   edit(element: any) {
-    console.log("element ",element);
-    
-    this.apiservice.editUser(element, element).subscribe((data) => {
-      console.log("edit Content", data);
-      this.fetchUser();
-    })
-    
+   
+  edit(element: any) {
+    console.log("Edit Content", element._id);
+    console.log("Edit Content", element);
+    this.router.navigate(['/edit', element]);
   }
-
   onView(element: any) {
-    this.apiservice.viewUser(element._id).subscribe((data) => {
-      console.log("view Content", data);
-      this.fetchUser();
-    })
+    const dialogRef = this.dialog.open(ViewModalDialog, {
+      width: '400px',
+      data: {
+        name: element.name,
+        email: element.email,
+        phoneNo: element.phoneNo
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      if (result === true) {
+        this.apiservice.viewUser(element._id).subscribe((data) => {
+          console.log("view Content", data);
+          this._snackBar.open('User Details','close',{
+            duration: 3000
+          });
+          this.fetchUser();
+        })
+      }
+    });
   }
 
   onDelete(element: any) {
@@ -115,3 +129,23 @@ export class TableComponent implements AfterViewInit {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModalDialog { }
+
+
+@Component({
+  selector: 'app-view-user-dialog',
+  styleUrl: './table.component.css',
+  templateUrl: './View.component.html',
+  imports: [MatDialogModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ViewModalDialog { 
+  constructor(
+    public dialogRef: MatDialogRef<ViewModalDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  // Close the dialog
+  closeDialog() {
+    this.dialogRef.close();
+  }
+ }
